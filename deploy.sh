cd3162cca507d04e0f9778763be1f291dd04369e
gbarnacle79 <gbarnacle79@gmail.com>
2021-11-13 13:37:01 +0000

    updated deploy.sh to include gunicorn

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo apt install python3 python3-pip python3-venv -y
-
+sudo apt install gunicorn
 python3 -m venv venv
 source venv/bin/activate
 
@@ -15,4 +15,7 @@ if [ ${create} ]; then
   python3 create.py
 fi
 
+python3 -m pytest --cov=application
+
+gunicorn -D --workers=4 --bind=0.0.0.0:5000 app:app
 python3 app.py
