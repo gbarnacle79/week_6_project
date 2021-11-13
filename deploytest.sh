e5905f168e6ab6823dd0bcc2730d5fd5b0d923c9
gbarnacle79 <gbarnacle79@gmail.com>
2021-11-13 20:15:00 +0000

    Altered deploy, to instead deploy the test file

---
+++ deploytest.sh
@@ -0,0 +1,23 @@
+sudo apt install python3 python3-pip python3-venv -y
+sudo apt install gunicorn
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+while getopts "c" options; do
+  case ${options} in
+    c) create=true;;
+  esac
+done
+
+if [ ${create} ]; then
+  python3 create.py
+fi
+
+python3 -m pytest --cov=application
+
+gunicorn -D --workers=4 --bind=0.0.0.0:5000 app:app
+#python3 app.py
+
+python3 -m pytest --cov=application --cov-report html
